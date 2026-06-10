d68b9091adacee215f78246e52a409cb66ddb368
Rayssa <rayssarrsilva1@gmail.com>
2026-06-10 14:39:55 -0300

    Add the function to save the input and close it
    when user click the bottom enter the input disappear and the value is
    saved, and the input is reseted, but value keeps save in another
    variable

--- javascript.js
+++ javascript.js
@@ -1,14 +1,28 @@
 const myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
 
 function addBookToLibrary() {}
 
 // Hover over the h3: My books, Logic.
 const shelfTitle = document.getElementById("shelf-title");
 const shelfQuestion = document.getElementById("shelf-question");
+const inputAmount = document.getElementById("booksAmount");
 
 shelfTitle.addEventListener("click", () => {
     shelfQuestion.classList.toggle("ativo");
 })
 
+inputAmount.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const value = inputAmount.value.trim();
+    }
+    shelfQuestion.classList.toggle("ativo");
+    inputAmount.value = "";
+})
