a608258570d8f816261530f60bc216a1b98fc8f4
Rayssa <rayssarrsilva1@gmail.com>
2026-06-10 13:54:28 -0300

    Do the logic of the my books
    when you click the my books title appear a input and when you click it
    back, it disappear

--- javascript.js
+++ javascript.js
@@ -3,3 +3,12 @@ const myLibrary = [];
 function Book() {}
 
 function addBookToLibrary() {}
+
+// Hover over the h3: My books, Logic.
+const shelfTitle = document.getElementById("shelf-title");
+const shelfQuestion = document.getElementById("shelf-question");
+
+shelfTitle.addEventListener("click", () => {
+    shelfQuestion.classList.toggle("ativo");
+})
+
